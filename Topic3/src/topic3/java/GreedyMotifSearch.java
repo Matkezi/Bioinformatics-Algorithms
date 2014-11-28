bdefbf0aee398f5ed1e283a67bcb3f95573e3ce0
matkezicro <matkezicro@gmail.com>
2014-11-28 19:31:55 +0100

    initial commit

---
+++ Topic3/src/topic3/java/GreedyMotifSearch.java
@@ -0,0 +1,49 @@
+package topic3.java;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * @author Matko
+ * @version 1.0
+ */
+public class GreedyMotifSearch {
+
+    int k = 1;
+    double[][] profile = new double[4][k];
+
+//    private String profileMostProbableKmer (String text, int k){
+//
+//    }
+
+
+    public static void main (String[] args) throws IOException{
+        File dir = new File("C:\\Users\\Matko\\IntelliJProjects\\Bioinformatics-Algorithms\\Topic3\\src\\topic3\\resources");
+        File file1 = new File(dir, "Profile-most Probable k-mer Problem.txt");
+        Path filepath = file1.toPath();
+
+        List<String> lines = Files.readAllLines(filepath);
+
+        String text = lines.get(0);
+        int k = Integer.parseInt(lines.get(1));
+
+        GreedyMotifSearch gms = new GreedyMotifSearch();
+
+        gms.k = k;
+
+        for (int i = 0; i<4;i++){
+            String line = lines.get(i+2);
+            String[] splited = line.split("\\s+");
+            for (int j = 0;j<gms.k;j++){
+                gms.profile[i][j] = Double.parseDouble(splited[j]);
+            }
+        }
+
+        System.out.print(gms.profile);
+    }
+
+
+}
