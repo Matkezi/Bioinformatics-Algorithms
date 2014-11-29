5b35d01d662bc7371de549a8a9e0946452c8a689
matkezicro <matkezicro@gmail.com>
2014-11-29 02:17:15 +0100

    works for simple example

--- Topic3/src/topic3/java/GreedyMotifSearch.java
+++ Topic3/src/topic3/java/GreedyMotifSearch.java
@@ -42,7 +42,7 @@ public class GreedyMotifSearch {
 
         //greedyMotifSearch();
 
-        for (int i = 0;i<1000;i++) {
+        for (int i = 0;i<10000;i++) {
 
             randomizedMotifSearch();
         }
