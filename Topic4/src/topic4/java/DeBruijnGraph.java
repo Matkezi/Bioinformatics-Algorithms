fb1f26eb17a79040c890a92c3e1579076dffea95
matkezicro <matkezicro@gmail.com>
2014-12-11 22:58:05 +0100

    DeBruijnGraph started

--- Topic4/src/topic4/java/DeBruijnGraph.java
+++ Topic4/src/topic4/java/DeBruijnGraph.java
@@ -1,10 +1,28 @@
 package topic4.java;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 /**
  * @author Matko
  * @version 1.0
  */
 public class DeBruijnGraph {
 
+    public static void execute() throws IOException{
+
+        File dir = new File("C:\\Users\\Matko\\IntelliJProjects\\Bioinformatics-Algorithms\\Topic4\\src\\topic4\\resources");
+        File file1 = new File(dir, "DeBruijn.txt");
+        Path filepath = file1.toPath();
+
+        List<String> lines = Files.readAllLines(filepath);
+
+        StringComposition sc = new StringComposition(Integer.parseInt(lines.get(0))-1,lines.get(1));
+        sc.makeComposition();
+
+    }
 
 }
--- Topic4/src/topic4/java/Main.java
+++ Topic4/src/topic4/java/Main.java
@@ -1,12 +1,16 @@
 package topic4.java;
 
+import java.io.IOException;
+
 /**
  * @author Matko
  * @version 1.0
  */
 public class Main {
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws IOException{
+
+        DeBruijnGraph.execute();
 
     }
 }
