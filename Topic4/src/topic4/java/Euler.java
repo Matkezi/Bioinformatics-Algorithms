bb5dad86729ba8068b1af162dd75c88f85e57ee6
matkezicro <matkezicro@gmail.com>
2014-12-12 14:27:16 +0100

    refactoringfd-.gvdfeyf

--- Topic4/src/topic4/java/Euler.java
+++ Topic4/src/topic4/java/Euler.java
@@ -4,10 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @author Matko
@@ -15,91 +12,38 @@ import java.util.Random;
  */
 public class Euler {
 
+    private class Edge {
+
+        String edgeName;
+
+
+    }
+
+    private HashMap<Edge, List<Edge>> graph = new HashMap<>();
+
     private List<String> lines = new ArrayList<>();
-    private HashMap<String,List<String>> graph = new HashMap<>();
-    private List<String> visitedEdges = new ArrayList<>();
-    private List<String> cycle = new ArrayList<>();
 
     private void formGraph(){
         for (String line : lines){
             List<String> rightSide = new ArrayList<>();
             String[] lineSplit = line.split("->");
-            if (lineSplit[1].contains(",")){
+            if (lineSplit[1].contains(",")) {
                 String[] toNodes = lineSplit[1].split(",");
-                for (String toNode : toNodes){
+                for (String toNode : toNodes) {
                     rightSide.add(toNode.trim());
                 }
             } else rightSide.add(lineSplit[1].trim());
-            graph.put(lineSplit[0].trim(),rightSide);
+
+            graph.put(lineSplit[0].trim(),null);
+
+
+
+
         }
-    }
-
-    private void refreshUnExploredEdges(){
-
     }
 
     private void findEulerCycle(){
-        HashMap<String,List<String>> unExploredEdges = new HashMap<>(graph);
 
-        List<String> keysAsArray = new ArrayList<>(graph.keySet());
-        Random r = new Random();
-
-        String currentEdge = keysAsArray.get(r.nextInt(keysAsArray.size()));
-        cycle.add(currentEdge);
-        String next ="";
-        for (String possibleNext : graph.get(currentEdge)){
-            if (!cycle.contains(possibleNext)){
-                next = possibleNext;
-                break;
-            }
-        }
-
-        while(!cycle.contains(next)){
-            currentEdge = next;
-            cycle.add(currentEdge);
-
-            String previousEdge = cycle.get(cycle.size()-2);
-            for (int i = 0; i<unExploredEdges.get(previousEdge).size();i++){
-                if (currentEdge.equals(unExploredEdges.get(previousEdge).get(i))){
-                    unExploredEdges.get(previousEdge).remove(i);
-                    if (unExploredEdges.get(previousEdge).size() == 0){
-                        unExploredEdges.remove(previousEdge);
-                        break;
-                    }
-                }
-            }
-
-            for (String possibleNext : graph.get(currentEdge)){
-                if (!cycle.contains(possibleNext)){
-                    next = possibleNext;
-                    break;
-                }
-            }
-        }
-
-        currentEdge = next;
-        for (String possibleNext : graph.get(currentEdge)){
-            next = possibleNext;
-            break;
-        }
-        currentEdge = next;
-        String previousEdge = cycle.get(cycle.size()-1);
-        for (int i = 0; i<unExploredEdges.get(previousEdge).size();i++){
-            if (currentEdge.equals(unExploredEdges.get(previousEdge).get(i))){
-                unExploredEdges.get(previousEdge).remove(i);
-                if (unExploredEdges.get(previousEdge).size() == 0){
-                    unExploredEdges.remove(previousEdge);
-                    break;
-                }
-            }
-        }
-
-        System.out.println(cycle);
-
-
-//        while(!unExploredEdges.isEmpty()){
-//
-//        }
 
     }
 
@@ -110,9 +54,8 @@ public class Euler {
         Path filepath = file1.toPath();
 
         lines = Files.readAllLines(filepath);
-        formGraph();
         findEulerCycle();
-
+        formGraph();
 
     }
 
