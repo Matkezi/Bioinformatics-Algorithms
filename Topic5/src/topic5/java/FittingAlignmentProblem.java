b4d6dd843d1f0744e4c7432b193b81b8746b44b2
matkezicro <matkezicro@gmail.com>
2015-01-04 14:17:06 +0100

    messed up with aligment

--- Topic5/src/topic5/java/FittingAlignmentProblem.java
+++ Topic5/src/topic5/java/FittingAlignmentProblem.java
@@ -40,7 +40,7 @@ public class FittingAlignmentProblem extends GlobalAlignmentProblem {
     protected void findAlignment(int i, int j){
 
 
-        if (i == 0 && j == 0){//only when we reach the end of matrix we can end the reursion
+        if (i == 0 && j == 0){//only when we reach the end of matrix we can end the recursion
             return;
         } else {//if we are in row 0 we must go left, and if we are in column 0 we must go up
             if (i == 0) backtrack[i][j] = "right";
@@ -70,6 +70,8 @@ public class FittingAlignmentProblem extends GlobalAlignmentProblem {
                 break;
 
             default:
+                vOut.add(v[i-1]);
+                wOut.add(w[j-1]);
                 return;
 
         }
