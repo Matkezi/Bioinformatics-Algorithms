4c43ff02a35f541e613a2cde7f73f8c3a5dc841c
matkezicro <matkezicro@gmail.com>
2015-01-04 17:20:40 +0100

    Levenshtein DONE!!!

--- Topic5/src/topic5/java/Levenshtein.java
+++ Topic5/src/topic5/java/Levenshtein.java
@@ -14,6 +14,16 @@ public class Levenshtein extends GlobalAlignmentProblem {
         backtrack = new String[v.length+1][w.length+1];
         s = new int[v.length+1][w.length+1];
 
+        //initialiaze first row
+        for (int j = 0;j<w.length;j++){
+            s[0][j+1] = s[0][j]-1;
+        }
+
+        //initialiaze first collumn
+        for (int i = 0;i<v.length;i++){
+            s[i+1][0] = s[i][0]-1;
+        }
+
         for (int i =1;i<v.length+1;i++) {
             for (int j = 1; j < w.length + 1; j++) {
 
@@ -21,7 +31,7 @@ public class Levenshtein extends GlobalAlignmentProblem {
                 values.add(s[i - 1][j] - 1);
                 values.add(s[i][j - 1] - 1);
 
-                if (v[i - 1].equals(w[j - 1])) values.add(s[i - 1][j - 1] + 1);
+                if (v[i - 1].equals(w[j - 1])) values.add(s[i - 1][j - 1]);
                 else values.add(s[i - 1][j - 1] - 1);
 
                 s[i][j] = Collections.max(values);
@@ -56,7 +66,19 @@ public class Levenshtein extends GlobalAlignmentProblem {
         formBacktrack();
         findAlignment(v.length, w.length);
         score = s[v.length][w.length];
-        System.out.println(score);
+        System.out.println(Math.abs(score));
+
+
+        Collections.reverse(vOut);
+        Collections.reverse(wOut);
+
+        for (int i = 0;i<vOut.size();i++){
+            System.out.print(vOut.get(i));
+        }
+        System.out.println();
+        for (int i = 0;i<wOut.size();i++){
+            System.out.print(wOut.get(i));
+        }
 
     }
 
