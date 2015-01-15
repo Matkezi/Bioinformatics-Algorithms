c702386844cad6fb14acd609b5581e87d6b0a21a
matkezicro <matkezicro@gmail.com>
2015-01-15 16:50:17 +0100

    chromosomeToCycle done

--- Topic6/src/topic6/java/Main.java
+++ Topic6/src/topic6/java/Main.java
@@ -8,6 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         TwoBreakDistance tbd = new TwoBreakDistance();
-        tbd.execute("");
+        tbd.execute("TwoBreakDistance.txt");
     }
 }
--- Topic6/src/topic6/java/TwoBreakDistance.java
+++ Topic6/src/topic6/java/TwoBreakDistance.java
@@ -1,6 +1,7 @@
 package topic6.java;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,6 +11,25 @@ import java.util.List;
 public class TwoBreakDistance extends LoadAndExecute {
 
     List<Integer> p = new ArrayList<>();
+
+    private List<Integer> chromosomeToCycle(List<Integer> chromosome){
+        Integer[] nodes = new Integer[1+chromosome.size()*2];
+
+        for (int j = 0;j<chromosome.size();j++){
+            int i = chromosome.get(j);
+            if (i > 0){
+                nodes[2*(j+1)-1] = 2*i-1;
+                nodes[2*(j+1)] = 2*i;
+            } else {
+                nodes[2*(j+1)-1] = -2*i;
+                nodes[2*(j+1)] = -2*i-1;
+            }
+        }
+
+        List<Integer> nodesL = new ArrayList<>(Arrays.asList(nodes));
+        nodesL.remove(0);
+        return nodesL ;
+    }
 
     @Override
     public void execute(String fileName) {
@@ -21,5 +41,7 @@ public class TwoBreakDistance extends LoadAndExecute {
         inputLine = inputLine.replaceAll("\\)","");
         String[] inputLineArray = inputLine.split(" ");
         for (String lineComp : inputLineArray) p.add(Integer.parseInt(lineComp));
+
+        System.out.println(chromosomeToCycle(p));
     }
 }
